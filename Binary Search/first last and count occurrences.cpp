d6a31376961d8aaa63807d6da83ed6d0d9ad3f23
Malay <malaykasana59@gmail.com>
2026-09-08 18:10:08 +0530

    a bit easy

--- Binary Search/first last and count occurrences.cpp
+++ Binary Search/first last and count occurrences.cpp
@@ -7,7 +7,7 @@ void mymethod(const vector<int>& arr, int x){
     int low{}, high{static_cast<int>(arr.size())-1};
     int cnt{},findex{-1},lindex{-1};
     while(low<=high){
-        int mid{(low+high)/2};
+        int mid{low+(high-low)/2};
         if (x==arr[mid]){
             cnt++;
             findex=lindex=mid;
@@ -16,7 +16,7 @@ void mymethod(const vector<int>& arr, int x){
                 cnt++;
                 findex=l--;
             }
-            while(h<arr.size() && arr[h]==arr[mid]){
+            while(h<static_cast<int>(arr.size()) && arr[h]==arr[mid]){
                 cnt++;
                 lindex=h++;
             }
@@ -24,7 +24,7 @@ void mymethod(const vector<int>& arr, int x){
         } else if (x>arr[mid]) low=mid+1;
         else high=mid-1;
     }
-    cout<<"first occurance of "<<x<<" is at "<<findex<<"\nlast occurance at "<<lindex<<"\nand it occurs "<<cnt<<" number of times";
+    cout<<"first occurrence of "<<x<<" is at "<<findex<<"\nlast occurrence at "<<lindex<<"\nand it occurs "<<cnt<<" number of times";
     return;
 }
 
@@ -32,17 +32,33 @@ void think(const vector<int>& arr, int x){
     int n{static_cast<int>(arr.size())};
     int lb=lower_bound(arr.begin(),arr.end(),x)-arr.begin();
     int ub=upper_bound(arr.begin(),arr.end(),x)-arr.begin()-1;
-    if (lb==n || arr[lb] != x) cout<<"ub and lb are {-1,-1}";
-    cout<<lb<<'\n'<<ub;
+    if (lb==n || arr[lb] != x) cout<<"first occurrence and last occurrence are {-1,-1} and count is 0";
+    else cout<<"first occurrence at : "<<lb<<'\n'<<"last occurrence at : "<<ub<<"\nthe count is "<<ub-lb+1;
 }
 
-void brute(const vector<int>& arr, int x){
+void optimal(const vector<int>& arr, int x){
     int low{};
     int high{static_cast<int>(arr.size()) - 1};
     int findex{-1};
     int lindex{-1};
     int cnt{};
 
+    while (low<=high){
+        int mid{low+(high-low)/2};
+        if (arr[mid]==x){
+            findex=mid;
+            high=mid-1;
+        } else if(x<arr[mid]) high=mid-1;
+        else low=mid+1;
+    }
+
+    if (findex==-1) {
+        cout << "first and last occurrence are {-1,-1}";
+        cout << "\ncount is 0";
+        return;
+    }
+    
+    low=0,high=static_cast<int>(arr.size())-1;
     while (low <= high) {
         int mid = low + (high - low) / 2;
         if (arr[mid] == x) {
@@ -52,18 +68,8 @@ void brute(const vector<int>& arr, int x){
         else low = mid + 1;
     }
 
-    low=0,high=static_cast<int>(arr.size())-1;
-    while (low<=high){
-        int mid{low+(high-low)/2};
-        if (arr[mid]==x){
-            findex=mid;
-            high=mid-1;
-        } else if(x<arr[mid]) high=mid-1;
-        else low=mid+1;
-    }
-    if (findex == -1) cnt = 0;
-    else cnt = lindex - findex + 1;
-    cout<<"first occurance of "<<x<<" is at "<<findex<<"\nlast occurance at "<<lindex<<"\nand it occurs "<<cnt<<" number of times";
+    cnt = lindex - findex + 1;
+    cout<<"first occurrence of "<<x<<" is at "<<findex<<"\nlast occurrence at "<<lindex<<"\nand it occurs "<<cnt<<" number of times";
     return;
 }
 
@@ -71,7 +77,7 @@ int main(){
     vector vec{3, 4, 13, 13, 13, 20, 40};
     int target{13};
     // mymethod(vec, target);
-    // brute(vec, target);
+    // optimal(vec, target);
     think(vec, target);
     return 0;
 }
