80d522f875c4c0a30d697794bf71cb107f61d74b
Malay <malaykasana59@gmail.com>
2026-09-09 17:27:03 +0530

    medium

--- Binary Search/_8minimum in rotated sorted arr.cpp
+++ Binary Search/_8minimum in rotated sorted arr.cpp
@@ -2,6 +2,18 @@
 #include<vector>
 using namespace std;
 
+int bs_diff(const vector<int>& arr){
+    int low{}, high{static_cast<int>(arr.size())-1};
+    int minimum=2147483647; //INT_MAX
+    while (low<=high){
+        if (low==high) return arr[low];
+        int mid{low+(high-low)/2};
+        if (arr[mid]<=arr[high]) high=mid;
+        else low=mid+1;
+    }
+    return -1;
+}
+
 int bs_min(const vector<int>& arr){
     int low{}, high{static_cast<int>(arr.size())-1};
     int minimum=2147483647; //INT_MAX
@@ -25,7 +37,8 @@ int bs_min(const vector<int>& arr){
 int main(){
     // vector vec{2,1};
     vector vec{4,5,6,7,0,1,2,3};
-    int minimum{bs_min(vec)};
+    // int minimum{bs_min(vec)};
+    int minimum{bs_diff(vec)};
     cout<< minimum;
     return 0;
 }
