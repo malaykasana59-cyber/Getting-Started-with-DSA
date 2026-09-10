edfaa97cfa570bc540bae8aae08c4db4016fafad
Malay <malaykasana59@gmail.com>
2026-09-10 18:23:01 +0530

    improved clarity

--- Binary Search/_11peak element in arr.cpp
+++ Binary Search/_11peak element in arr.cpp
@@ -18,8 +18,8 @@ void brute(const vector<int>& arr, int n){
 
 int bs_peak(const vector<int>& arr, int n){
     int low{1}, high{n-2};
-    if (arr[0]>arr[1]) return arr[0];
-    if (arr[n-1]>arr[n-2]) return arr[n-1];
+    if (arr[0]>arr[1]) return 0;
+    if (arr[n-1]>arr[n-2]) return n-1;
     while(low<=high){
         int mid{low+(high-low)/2};
         if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
